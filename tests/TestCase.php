697660e8ee6e16833ed176eb3e90d89b935aabb8
sebapastore <sebastianerp@gmail.com>
2023-07-14 12:48:45 -0400

    New: Add test for create new customer

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
@@ -10,4 +11,12 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication;
     use RefreshDatabase;
 
+    public function login($user = null)
+    {
+        $user ??= User::factory()->create();
+
+        $this->actingAs($user);
+
+        return $user;
+    }
 }
