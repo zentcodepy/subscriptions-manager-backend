9e98114f84c92a03d43455be53aef8deba5be0d7
sebapastore <sebastianerp@gmail.com>
2023-07-14 12:44:45 -0400

    Adjust: Relocate auth routes and controllers

--- app/Http/Controllers/Api/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Api/Auth/AuthenticatedSessionController.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use function response;
 
 class AuthenticatedSessionController extends Controller
 {
