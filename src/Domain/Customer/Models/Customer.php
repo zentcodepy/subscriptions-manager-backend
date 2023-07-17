cb8d5db2a9018dada7a5a1c63e32cfb63d81cdef
sebapastore <sebastianerp@gmail.com>
2023-07-17 10:19:55 -0400

    Adjust: Add missing mixin to Models, related to IDE Helper

--- src/Domain/Customer/Models/Customer.php
+++ src/Domain/Customer/Models/Customer.php
@@ -5,6 +5,9 @@ namespace Domain\Customer\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperCustomer
+ */
 class Customer extends Model
 {
     use HasFactory;
