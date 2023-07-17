dcbc01653cc745369b3f1ac309541c87ff89eceb
sebapastore <sebastianerp@gmail.com>
2023-07-17 10:19:00 -0400

    New: Add laravel-ide-helper package to improve IDE autocompletion

--- README.md
+++ README.md
@@ -3,20 +3,19 @@
 ### Description
 * The purpose of this program is to provide a subscription manager backend system.
 
-### Instalation
+### Installation
 * Clone this repository
 * Create .env file with DB variables: DB_DATABASE, DB_USERNAME and DB_PASSWORD (don't use root for DB_USERNAME)
-* From project directory execute: docker-compose build app
-* From project directory execute: docker-compose up -d
-* From project directory execute: docker exec <container-name> composer install
-
-### Development 
-* php artisan serve
+* From project directory execute: ```docker-compose build app```
+* From project directory execute: ```docker-compose up -d```
+* From project directory execute: ```docker exec <container-name> composer install```
 
 ### Built With
 * [Laravel](https://laravel.com/)
 * [PHP](https://www.php.net/) - v8.1
 
+### IDE Helper
+* After adding a new Model, or after adding columns or relationships to an exisiting one, run: ```php artisan ide-helper:models -M```
 
 <p align="right">(<a href="#top">back to top</a>)</p>
 
---
+++ _ide_helper_models.php
@@ -0,0 +1,77 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property mixed $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+	class IdeHelperUser {}
+}
+
+namespace Domain\Customer\Models{
+/**
+ * Domain\Customer\Models\Customer
+ *
+ * @property int $id
+ * @property string $business_name
+ * @property string $document_number
+ * @property string $contact_name
+ * @property string $phone_number
+ * @property string $email
+ * @property string $address
+ * @property string $comments
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereBusinessName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereComments($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereContactName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereDocumentNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer wherePhoneNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Customer whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+	class IdeHelperCustomer {}
+}
+
