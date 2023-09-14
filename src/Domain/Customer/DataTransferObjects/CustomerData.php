5bde956d02d25801385d0c21728345389bf89ea8
sebapastore <sebastianerp@gmail.com>
2023-09-14 10:43:35 -0400

    Refactor: use Attributes for customer data validation

--- src/Domain/Customer/DataTransferObjects/CustomerData.php
+++ src/Domain/Customer/DataTransferObjects/CustomerData.php
@@ -4,6 +4,10 @@ namespace Domain\Customer\DataTransferObjects;
 
 use Illuminate\Http\Request;
 use Spatie\LaravelData\Attributes\MapInputName;
+use Spatie\LaravelData\Attributes\Validation\Nullable;
+use Spatie\LaravelData\Attributes\Validation\Required;
+use Spatie\LaravelData\Attributes\Validation\Sometimes;
+use Spatie\LaravelData\Attributes\Validation\StringType;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 
@@ -12,33 +16,31 @@ class CustomerData extends Data
 {
     public function __construct(
         public readonly ?int $id,
+
+        #[Required, StringType]
         public readonly string $businessName,
+
+        #[Nullable, Sometimes, StringType]
         public readonly ?string $documentNumber,
+
+        #[Nullable, Sometimes, StringType]
         public readonly ?string $contactName,
+
+        #[Nullable, Sometimes, StringType]
         public readonly ?string $phoneNumber,
+
+        #[Nullable, Sometimes, StringType]
         public readonly ?string $email,
+
+        #[Nullable, Sometimes, StringType]
         public readonly ?string $address,
+
+        #[Nullable, Sometimes, StringType]
         public readonly ?string $comments,
     ) {}
 
     public static function fromRequest(Request $request): self
     {
         return self::from($request->all());
-    }
-
-    /**
-     * @return array<string, array<int, string>>
-     */
-    public static function rules(): array
-    {
-        return [
-            'business_name' => ['required', 'string'],
-            'document_number' => ['nullable', 'sometimes', 'string'],
-            'contact_name' => ['nullable', 'sometimes', 'string'],
-            'phone_number' => ['nullable', 'sometimes', 'string'],
-            'email' => ['nullable', 'sometimes', 'email'],
-            'address' => ['nullable', 'sometimes', 'string'],
-            'comments' => ['nullable', 'sometimes', 'string'],
-        ];
     }
 }
