6be3597e1db962d37362971589bdb43fbd907a34
sebapastore <sebastianerp@gmail.com>
2023-09-28 10:22:45 -0400

    Refactor: use Attributes in al DTOs for validation

--- src/Domain/Service/DataTransferObjects/ServiceUpdateData.php
+++ src/Domain/Service/DataTransferObjects/ServiceUpdateData.php
@@ -6,33 +6,33 @@ use Domain\Service\Helpers\ServiceStatus;
 use Illuminate\Http\Request;
 use Spatie\LaravelData\Attributes\MapInputName;
 use Spatie\LaravelData\Attributes\Validation\Enum;
+use Spatie\LaravelData\Attributes\Validation\IntegerType;
+use Spatie\LaravelData\Attributes\Validation\Nullable;
+use Spatie\LaravelData\Attributes\Validation\Sometimes;
+use Spatie\LaravelData\Attributes\Validation\StringType;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Mappers\SnakeCaseMapper;
+use Symfony\Contracts\Service\Attribute\Required;
 
 #[MapInputName(SnakeCaseMapper::class)]
 class ServiceUpdateData extends Data
 {
     public function __construct(
+        #[Nullable, Sometimes, StringType]
         public readonly ?int $id,
+
+        #[Required, StringType]
         public readonly string $name,
+
+        #[Required, IntegerType]
         public readonly int $customerId,
+
+        #[Required, Enum(ServiceStatus::class)]
         public readonly string $status,
     ) {}
 
     public static function fromRequest(Request $request): self
     {
         return self::from($request->all());
-    }
-
-    /**
-     * @return array<string, array<int, mixed>>
-     */
-    public static function rules(): array
-    {
-        return [
-            'name' => ['required', 'string'],
-            'customer_id' => ['required', 'integer'],
-            'status' => ['required', new Enum(ServiceStatus::class)],
-        ];
     }
 }
