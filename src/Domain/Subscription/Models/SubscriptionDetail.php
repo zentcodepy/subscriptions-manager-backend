e6135a5387360af4cc527be251346e687923269f
sebapastore <sebastianerp@gmail.com>
2023-09-28 17:30:30 -0400

    Add date casting to payed_at in DusbscriptionDetail

--- src/Domain/Subscription/Models/SubscriptionDetail.php
+++ src/Domain/Subscription/Models/SubscriptionDetail.php
@@ -16,6 +16,15 @@ class SubscriptionDetail extends Model
     protected $guarded = [];
 
     /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'payed_at' => 'date:Y-m-d H:i:s',
+    ];
+
+    /**
      * @return BelongsTo<Subscription, SubscriptionDetail>
      */
     public function subscription(): BelongsTo
