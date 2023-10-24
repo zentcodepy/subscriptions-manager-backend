c472673052f6d7a15582c92bc7941b8b4d326fc3
sebapastore <sebastianerp@gmail.com>
2023-10-24 11:05:28 -0300

    New: add search endpoint for services

--- routes/api/service.php
+++ routes/api/service.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Api\Service\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('customers/search', [ServiceController::class, 'search']);
+
     Route::resource('services', ServiceController::class)
         ->only(['index', 'store', 'show', 'update', 'destroy']);
 });
