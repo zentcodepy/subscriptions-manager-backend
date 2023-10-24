8e42cbaa35937c1607b1dcbdda50ab743d10e91c
sebapastore <sebastianerp@gmail.com>
2023-10-24 11:32:21 -0300

    Add names to search routes

--- routes/api/service.php
+++ routes/api/service.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\Api\Service\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('customers/search', [ServiceController::class, 'search']);
+    Route::get('service/search', [ServiceController::class, 'search'])
+        ->name('services.search');
 
     Route::resource('services', ServiceController::class)
         ->only(['index', 'store', 'show', 'update', 'destroy']);
